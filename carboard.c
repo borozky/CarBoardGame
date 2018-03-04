53ea66667e0d185c37d707d9ea952339f3d78552
rmit-s3485376-joshua-orozco <s3485376@student.rmit.edu.au>
2018-03-04 18:21:32 +1100

    Menu selection

--- carboard.c
+++ carboard.c
@@ -6,15 +6,54 @@
 
 #include "carboard.h"
 
-int main()
+int main(void)
 {
-    /* TODO */
-    printf("Good bye! \n\n");
+    int choice = 0;
+    showMenu();
+
+    while (choice == 0 && choice <= 3) {
+        printf("Please enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            printf("You have chosen to play the game");
+            return EXIT_SUCCESS;
+        }
+
+        if (choice == 2) {
+            showStudentInformation();
+            choice = 0;
+            continue;
+        }
+
+        if (choice == 3) {
+            printf("You have chosen to quit");
+            return EXIT_SUCCESS;
+        }
+    }
 
     return EXIT_SUCCESS;
+        
+
+    
 }
+
+void showMenu() {
+    printf("\n");
+    printf("Welcome to Car Board\n");
+    printf("--------------------\n");
+    printf("1. Play game\n");
+    printf("2. Show student's information\n");
+    printf("3. Quit\n");
+    printf("\n");
+}
+
 
 void showStudentInformation()
 {
-    /* TODO */
+    printf("------------------------------------\n");
+    printf("Name: Joshua Orozco\n");
+    printf("No: s3485376\n");
+    printf("s3485376@student.rmit.edu.au\n");
+    printf("------------------------------------\n");
 }
